a9e03e109f9ade1d89d74456d74221436327e714
Wojciech Banas <fizista@gmail.com>
2012-11-22 21:50:30 +0100

    add QuerySet.delete(), still needed fixes

--- repchan/managers.py
+++ repchan/managers.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8
 
 from django.db import models
+from django.db.models.query import QuerySet
 
 class RawManager(models.Manager):
     '''
@@ -24,6 +25,27 @@ class VersionManager(models.Manager):
         return self.get_query_set().filter(version_parent_rev_pk=revision,
                                            version_unique_on__isnull=False)
 
+class VersionQuerySet(models.query.QuerySet):
+
+    def delete(self, hard=False, *args, **kwargs):
+        # removed from the database or move to trash
+        if self.version_in_trash or hard:
+            # without sending signals post_delete, pre_delete
+            super(VersionQuerySet, self).delete(*args, **kwargs)
+        elif hard:
+            # with sending signals
+            signal_allow(self, models.signals.pre_delete)
+            signal_allow(self, models.signals.post_delete)
+
+            super(VersionQuerySet, self).delete(*args, **kwargs)
+
+            signal_allow(self, models.signals.pre_delete, count=0)
+            signal_allow(self, models.signals.post_delete, count=0)
+        else:
+            pass
+        # TODO: QuerySet.delete collector
+        # TODO: Disable delete context
+
 
 class DefaultManager(models.Manager):
     '''
@@ -32,7 +54,7 @@ class DefaultManager(models.Manager):
     #use_for_related_fields = True # WARNING: errors
 
     def get_query_set(self):
-        return super(DefaultManager, self).get_query_set().\
+        return VersionQuerySet(self.model, using=self._db).\
                         filter(version_in_trash=False, version_unique_on=False)
 
     def in_trash(self):
@@ -41,3 +63,11 @@ class DefaultManager(models.Manager):
         '''
         return super(DefaultManager, self).get_query_set().\
                         filter(version_in_trash=True, version_unique_on=False)
+
+    def together_with_trash(self):
+        '''
+        list of objects in the trash
+        '''
+        return super(DefaultManager, self).get_query_set().\
+                        filter(version_unique_on=False)
+
